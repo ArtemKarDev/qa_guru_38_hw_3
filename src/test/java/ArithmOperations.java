301c532b38854fa4cda261b682ece3cabe71c4b4
ArtemKarDev <akm86@mail.ru>
2025-10-11 17:50:51 +0300

    arithmeticOperations

--- src/test/java/ArithmOperations.java
+++ src/test/java/ArithmOperations.java
@@ -6,11 +6,7 @@ public class ArithmOperations {
     public static int division(int a, int b) {return a / b;}
     public static int multiply(int a, int b) {return a * b;}
 
-    public double arithmeticOperations(int a, int b, double x, double y) {
-        return a + x * b - y;
-    }
-
-    public void doublelogicOperations(int a, int b, double x, double y) {
+    public double compareOperations(int a, int b, double x, double y) {
 
         boolean result1 = a > b;
         boolean result2 = x < y;
@@ -21,10 +17,13 @@ public class ArithmOperations {
         System.out.println("x < y: " + result2);
         System.out.println("a >= 10: " + result3);
         System.out.println("x <= 7.5: " + result4);
+
+        return a + x * b - y;
     }
 
+
     public void overflowExample() {
-        Byte y = 125;
+        byte y = 125;
         for (byte i = 1; i <= 4; i++) {
             ++y;
             System.out.print(" " + y);
@@ -36,7 +35,9 @@ public class ArithmOperations {
         System.out.println("пример суммы " + ArithmOperations.sum(4,5));
         System.out.println("пример разницы " + ArithmOperations.minus(4,5));
         System.out.println("пример частного " + ArithmOperations.division(4,5));
-        System.out.println("пример умножения " + ArithmOperations.sum(4,5));
+        System.out.println("пример умножения " + ArithmOperations.multiply(4,5));
+        System.out.println("пример сравнений ");
+        compareOperations(2,3,4.4,8.8);
         System.out.print("пример с переполнением ");
         overflowExample();
     }
